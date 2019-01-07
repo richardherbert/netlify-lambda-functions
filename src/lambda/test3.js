a8e2d5e14941bbc0a187d2448ce91d77f2ce5fa8
Richard Herbert <richard@infoweb.co.uk>
2019-01-07 10:59:23 +0000

    Simplified function

--- src/lambda/test3.js
+++ src/lambda/test3.js
@@ -5,10 +5,6 @@ const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/todos/1';
 exports.handler = async ( event, context ) => {
 	return fetch( API_ENDPOINT )
 		.then( response => response.json() )
-		.then( data => ( {
-			statusCode: 200,
-			body: `${data}`
-		} ) )
 		.catch( error => ( {
 			statusCode: 422,
 			body: String( error )
