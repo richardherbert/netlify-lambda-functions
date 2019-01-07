a1eba1730dce2121b217b13d941c9ce0067d4c75
Richard Herbert <richard@infoweb.co.uk>
2019-01-07 11:18:20 +0000

    try and return a value

--- src/lambda/test3.js
+++ src/lambda/test3.js
@@ -5,6 +5,10 @@ const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/todos/1';
 exports.handler = async ( event, context ) => {
 	return fetch( API_ENDPOINT )
 		.then( response => response.json() )
+		.then( data => ( {
+			statusCode: 200,
+			body: `${data.title}`
+		} ) )
 		.catch( error => ( {
 			statusCode: 422,
 			body: String( error )
