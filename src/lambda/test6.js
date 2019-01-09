5a242c1c23898ecd6b1e79d4ee0f56650b1c4fbf
Richard Herbert <richard@infoweb.co.uk>
2019-01-09 15:28:25 +0000

    try a different structure

--- src/lambda/test6.js
+++ src/lambda/test6.js
@@ -2,19 +2,14 @@ const axios = require( 'axios' );
 const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/todos/1';
 
 exports.handler = ( event, context, callback ) => {
-	axios( {
-			method: 'get'
-			,url: 'https://jsonplaceholder.typicode.com/todos/1'
-			,responseType: 'json'
+	axios.get( API_ENDPOINT, {
+			responseType: 'json'
 		}
 	)
 		.then( ( response ) => {
-			console.log(response.data);
-
 			callback( null, {
 				statusCode: 200,
 				body: JSON.stringify( response.data )
-				// body: response.data
 			} );
 		} )
 		.catch( ( error ) => {
