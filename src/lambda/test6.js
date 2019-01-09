ee880ef6904a28603ec9b06e89d68dbac66dd259
Richard Herbert <richard@infoweb.co.uk>
2019-01-09 15:15:03 +0000

    change format

--- src/lambda/test6.js
+++ src/lambda/test6.js
@@ -2,7 +2,12 @@ const axios = require( 'axios' );
 const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/todos/1';
 
 exports.handler = ( event, context, callback ) => {
-	axios.get( API_ENDPOINT )
+	axios.get( {
+			method: 'get'
+			,url: API_ENDPOINT
+			,responseType: 'json'
+		}
+	)
 		.then( ( response ) => {
 			// const payload = JSON.parse( event.body );
 
